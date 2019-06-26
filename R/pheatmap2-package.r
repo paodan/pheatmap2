23ecb2125e6ec2f694351b908d14935c3d521431
paodan <weiyangtao1513@gmail.com>
2019-06-26 13:09:07 +0200

    message

---
+++ R/pheatmap2-package.r
@@ -0,0 +1,19 @@
+#' Pretty heatmaps
+#'
+#' Implementation of heatmaps that offers more control
+#' over dimensions and appearance.
+#'
+#' @name pheatmap2-package
+#' @docType package
+#'
+#' @import grid
+#' @import RColorBrewer
+#' @import scales
+#' @import gtable
+#' @import stats
+#' @import grDevices
+#' @import graphics
+#' @import cba
+#'
+#'
+NA
